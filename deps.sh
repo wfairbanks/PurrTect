d9eae8d4425da5f03bce7983deab03fb95c46eaf
wfairbanks <williamedwardfairbanks@gmail.com>
2023-08-28 15:34:39 +0100

    updated deps

--- deps.sh
+++ deps.sh
@@ -10,10 +10,3 @@ source purrvenv/bin/activate;
 
 # Install OpenCV and other Python libraries
 pip3 install opencv-python opencv-python-headless RPi.GPIO flask;
-
-# Activate Venv
-source purrvenv/bin/activate;
-
-#Launch App
-cd myapp;
-python3 app.py
