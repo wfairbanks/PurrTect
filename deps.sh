af33e7bb0bcd5889da8e15e61c71cb479a755682
wfairbanks <williamedwardfairbanks@gmail.com>
2023-08-28 15:33:16 +0100

    updated deps

--- deps.sh
+++ deps.sh
@@ -4,12 +4,6 @@
 sudo apt-get update;
 sudo apt-get install -y libopencv-dev python3-venv;
 
-# Clone GitHub Repo
-git clone https://github.com/wfairbanks/purrtect.git;
-
-# CD to repo
-cd purrtect;
-
 # Create project directory and setup virtual env
 python3 -m venv purrvenv;
 source purrvenv/bin/activate;
