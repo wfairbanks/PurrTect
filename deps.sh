89c4c640a0faa159f9476e95286c2d9e22a4332d
wfairbanks <williamedwardfairbanks@gmail.com>
2023-08-28 16:23:37 +0100

    ups

--- deps.sh
+++ deps.sh
@@ -1,16 +1,22 @@
-#/bin/bash
+#!/bin/bash
 
 # Update System
 sudo apt-get update;
-sudo apt-get install -y libopencv-dev python3-venv;
+sudo apt-get install -y python3-venv;
 
-# Create project directory and setup virtual env
+# Create a virtual environment
 python3 -m venv purrvenv;
 source purrvenv/bin/activate;
 
-# Install OpenCV and other Python libraries
-pip install --upgrade pip;
+# Upgrade pip for better performance and compatibility
 pip install --upgrade pip setuptools wheel;
-echo "deb http://www.piwheels.org/simple stretch main" | sudo tee -a /etc/apt/sources.list.d/piwheels.list;
+
+# Add piwheels for faster pip installations on Raspberry Pi
+echo "deb http://www.piwheels.org/simple stretch main" | sudo tee /etc/apt/sources.list.d/piwheels.list;
 sudo apt update;
-pip install opencv-python, opencv-python-headless RPi.GPIO flask;
+
+# Install OpenCV system dependencies
+sudo apt-get install -y libopencv-dev;
+
+# Install Python libraries using piwheels for faster installations
+pip install opencv-python opencv-python-headless RPi.GPIO flask;
